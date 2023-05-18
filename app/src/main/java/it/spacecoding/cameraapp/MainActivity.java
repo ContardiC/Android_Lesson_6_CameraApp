028eb4344b9b58afcef39ffbc2b043a32f653fd4
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-05-18 17:19:47 +0200

    Button and ImageView added

--- app/src/main/java/it/spacecoding/cameraapp/MainActivity.java
+++ app/src/main/java/it/spacecoding/cameraapp/MainActivity.java
@@ -3,12 +3,21 @@ package it.spacecoding.cameraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 101;
+
+    private Button mTakePhotoButton;
+    private ImageView mPhotoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTakePhotoButton = findViewById(R.id.take_photo_button);
+        mPhotoImageView = findViewById(R.id.photo_image_view);
     }
 }
